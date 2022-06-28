4743348182b6c217cb4aaf4887c63ce138acc1e8
Haiderali43707 <105425518+Haiderali43707@users.noreply.github.com>
2022-06-28 23:11:43 +0530

    Create Number of 1 Bits.java

---
+++ Number of 1 Bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int setBits(int n) {
+        
+        // code here
+        int count=0;
+        while(n>0)
+        {
+            count+=n&1;
+            n>>=1;
+        }
+        return count;
+    }
+}
